304c4c78e32dea974cd5998a423739faeb0b4bbc
PHLoc <phunghloc@gmail.com>
2020-09-12 18:16:27 +0700

    Updates

---
+++ precache-manifest.9549a616ed05190ca9db2c866fd1e2fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07a44972dd365a278a18983f96806155",
+    "url": "/xizach/index.html"
+  },
+  {
+    "revision": "1dbeba0e2b5ce78b18f3",
+    "url": "/xizach/static/css/main.1dbfb82d.chunk.css"
+  },
+  {
+    "revision": "04aaeae4a7cd8c33abd3",
+    "url": "/xizach/static/js/2.92c630b1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/xizach/static/js/2.92c630b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1dbeba0e2b5ce78b18f3",
+    "url": "/xizach/static/js/main.66955a0f.chunk.js"
+  },
+  {
+    "revision": "f755f40170f533bb7d99",
+    "url": "/xizach/static/js/runtime-main.13d2d5c4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/phunghloc/xizach.git/precache-manifest.e548a4cb0a91caca0dd6cb65ec7faafe.js"
+  "/xizach/precache-manifest.9549a616ed05190ca9db2c866fd1e2fe.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/phunghloc/xizach.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/xizach/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
