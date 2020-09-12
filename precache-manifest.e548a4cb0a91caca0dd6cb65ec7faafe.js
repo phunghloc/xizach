118e3bca34e41603ff73107756757264e024156d
PHLoc <phunghloc@gmail.com>
2020-09-12 18:01:39 +0700

    Updates

---
+++ precache-manifest.e548a4cb0a91caca0dd6cb65ec7faafe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ddbd3f3e4d504225f0feb096232048e",
+    "url": "/phunghloc/xizach.git/index.html"
+  },
+  {
+    "revision": "cd42d56afe5141ab678b",
+    "url": "/phunghloc/xizach.git/static/css/main.1dbfb82d.chunk.css"
+  },
+  {
+    "revision": "04aaeae4a7cd8c33abd3",
+    "url": "/phunghloc/xizach.git/static/js/2.92c630b1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/phunghloc/xizach.git/static/js/2.92c630b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd42d56afe5141ab678b",
+    "url": "/phunghloc/xizach.git/static/js/main.13e08122.chunk.js"
+  },
+  {
+    "revision": "51f42c62d642a3da7fee",
+    "url": "/phunghloc/xizach.git/static/js/runtime-main.afad0319.js"
+  }
+]);
